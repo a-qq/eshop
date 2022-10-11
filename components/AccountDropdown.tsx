00a7b4593aaa79721624f27ecbccce3f0f386b33
Robert Olech <robert_olech@o2.pl>
2022-10-12 01:12:26 +0200

    added account dropdown component

---
+++ components/AccountDropdown.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+
+export const AccountDropdown = ({ className = "" }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleEscape = (e: KeyboardEvent) => {
+    if (e.key === `Esc` || e.key === `Escape`) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleEscape);
+    return () => {
+      window.removeEventListener("keydown", handleEscape);
+    };
+  }, [open]);
+
+  return (
+    <div className={`relative ${className}`}>
+      <button
+        onClick={() => setOpen(!open)}
+        className="relative z-10 block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white"
+      >
+        <img
+          className="h-full w-full object-cover"
+          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+          alt="Your avatar"
+        />
+      </button>
+
+      {open ? (
+        <>
+          <button
+            onClick={() => setOpen(false)}
+            tabIndex={-1}
+            className="fixed inset-0 h-full w-full cursor-default"
+          ></button>
+          <div className="absolute right-0 mt-2 py-2 w-24 bg-white rounded-lg shadow-xl">
+            <a
+              href="#"
+              className="block px-4 py-2 text-gray-800 hover:bg-indigo-300 hover:text-text-gray-900"
+            >
+              Settings
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-gray-800 hover:bg-indigo-300 hover:text-text-gray-900"
+            >
+              Support
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-gray-800 hover:bg-indigo-300 hover:text-text-gray-900"
+            >
+              Sign out
+            </a>
+          </div>
+        </>
+      ) : null}
+    </div>
+  );
+};
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,19 @@
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
+import { AccountDropdown } from "./AccountDropdown";
 
 export const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <header className="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
+    <header className="bg-gray-800 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
       <div className="flex items-center justify-between px-4 py-3 sm:p-0">
         <div>
           <img className="h-8" src="/aqq_logo.svg" alt="a-qq logo" />
         </div>
         <div className="sm:hidden">
           <button
-            onClick={() => {
-              setOpen(!open);
-            }}
+            onClick={() => setOpen(!open)}
             type="button"
             className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
           >
@@ -26,32 +25,56 @@ export const Navbar = () => {
           </button>
         </div>
       </div>
-      <div className={`${open ? "block" : "hidden"} px-2 pt-2 pb-4 sm:flex sm:p-0`}>
-        <a
-          href="#"
-          className="block text-white font-semibold rounded px-2 py-1 hover:bg-gray-800"
-        >
-          Home
-        </a>
-        <a
-          href="#"
-          className="mt-1 block text-white font-semibold rounded px-2 py-1 hover:bg-gray-800 sm:mt-0 sm:ml-2"
-        >
-          About
-        </a>
-        <a
-          href="#"
-          className="mt-1 block text-white font-semibold rounded px-2 py-1 hover:bg-gray-800 sm:mt-0 sm:ml-2"
-        >
-          Collections
-        </a>
-        <a
-          href="#"
-          className="mt-1 block text-white font-semibold rounded px-2 py-1 hover:bg-gray-800 sm:mt-0 sm:ml-2"
-        >
-          Other
-        </a>
-      </div>
+      <nav className={`${open ? "block" : "hidden"} sm:block`}>
+        <div className="px-2 pt-2 pb-4 sm:flex sm:p-0">
+          <a
+            href="/"
+            className="block text-white font-semibold rounded px-2 py-1 hover:bg-gray-800"
+          >
+            Home
+          </a>
+          <a
+            href="/about"
+            className="mt-1 block text-white font-semibold rounded px-2 py-1 hover:bg-gray-800 sm:mt-0 sm:ml-2"
+          >
+            About
+          </a>
+          <a
+            href="#"
+            className="mt-1 block text-white font-semibold rounded px-2 py-1 hover:bg-gray-800 sm:mt-0 sm:ml-2"
+          >
+            Collections
+          </a>
+          <a
+            href="#"
+            className="mt-1 block text-white font-semibold rounded px-2 py-1 hover:bg-gray-800 sm:mt-0 sm:ml-2"
+          >
+            Other
+          </a>
+          <AccountDropdown className="hidden sm:block sm:ml-6" />
+        </div>
+        <div className="px-4 py-5 border-t border-gray-800 sm:hidden">
+          <div className="flex items-center">
+            <img
+              className="h-8 w-8 border-2 border-gray-600 rounded-full object-cover"
+              src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+              alt="Your avatar"
+            />
+            <span className="ml-3 font-semibold text-white">Adam Kowalski</span>
+          </div>
+          <div className="mt-4">
+            <a href="#" className="block text-gray-400 hover:text-white">
+              Settings
+            </a>
+            <a href="#" className="mt-2 block text-gray-400 hover:text-white">
+              Support
+            </a>
+            <a href="#" className="mt-2 block text-gray-400 hover:text-white">
+              Sign out
+            </a>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
