894ffebd4cb5ca7392a360081fa882baa9ddd6fe
Robert Olech <robert_olech@o2.pl>
2022-10-13 03:50:47 +0200

    styled footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 export const Footer = () => {
-    return (
-        <footer className="bg-gray-900 max-w-5xl mx-auto w-full text-white px-4 py-2">
-        Stopka
-      </footer>
-    );
-}
+  return (
+    <footer className="bg-gray-100">
+      <div className="max-w-screen-3xl px-4 pb-6 sm:px-6 lg:px-8">
+        <div className="border-t border-gray-200 pt-6">
+          <div className="text-center sm:flex sm:justify-between sm:text-left">
+            <p className="text-sm text-gray-500">
+              <span className="block sm:inline">All rights reserved. </span>
+              <Link href="/#">
+                <a className="inline-block text-teal-600 underline transition hover:text-teal-600/75">
+                  Terms & Conditions
+                </a>
+              </Link>
+              <span> &middot; </span>
+              <Link href="/#">
+                <a className="inline-block text-teal-600 underline transition hover:text-teal-600/75">
+                  Privacy Policy
+                </a>
+              </Link>
+            </p>
+            <p className="mt-4 text-sm text-gray-500 sm:order-first sm:mt-0">
+              &copy; 2022 a-qq
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
